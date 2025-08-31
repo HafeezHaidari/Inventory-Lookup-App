c5f84ab56f413d3830983e4293794d8d90424bb0
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-08-31 17:16:03 +0200

    Added first component: AppLogo.tsx

---
+++ shelfy-v1/components/AppLogo.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+const AppLogo = () => {
+    return (
+        <div className='flex justify-center items-center gap-4'>
+            <div className="relative w-20 h-20">
+                <Image
+                    src="/chat-logo.svg"
+                    alt="Inventory Search"
+                    fill
+                    priority
+                    className="object-contain"
+                />
+            </div>
+            <div>
+                <p className='text-4xl font-sans font-medium'>
+                    Inventory Search
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default AppLogo;
