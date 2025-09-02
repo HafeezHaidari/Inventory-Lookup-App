0ff168c098312b5c370ab7bbaadebab8abf155d8
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-02 14:49:06 +0200

    Added ProductCardPresenter component.

--- shelfy-v1/app/(home)/page.tsx
+++ shelfy-v1/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import AppLogo from "@/components/AppLogo";
 import SearchBar from "@/components/SearchBar";
+import ProductCardPresenter from "@/components/ProductCardPresenter";
 
 export default function Home() {
   return (
       <>
           <AppLogo />
           <SearchBar />
+          <ProductCardPresenter />
       </>
   );
 }
