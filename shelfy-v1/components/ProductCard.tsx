f593d679e7a8c7b9899cf95fe834c711d38fc10c
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-05 17:32:47 +0200

    Wrapped the card in a Link element.

--- shelfy-v1/components/ProductCard.tsx
+++ shelfy-v1/components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from "next/image";
+import Link from "next/link";
 import {Product} from "@/types/types";
 
 interface ProductCardProps {
@@ -8,20 +9,22 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return (
-        <div className="max-w-xs bg-white rounded-2xl shadow-md p-4 hover:shadow-lg hover:scale-105 transition-transform duration-300 text-center">
-            <Image
-                src={product.imageUrl}
-                alt={product.name}
-                width={100}
-                height={100}
-                className="w-full h-48 object-contain rounded-md mx-auto" />
-            <div className="mt-4">
-                <h2 className="text-lg font-semibold text-gray-900">{product.name}</h2>
-                <p className="text-sm text-gray-500">{product.brand}</p>
-                <p>{product.unit}</p>
-                <p className="mt-2 text-xl font-bold text-gray-900">{product.defaultPrice} €</p>
+        <Link href={"/"}>
+            <div className="max-w-xs bg-white rounded-2xl shadow-md p-4 hover:shadow-lg hover:scale-105 transition-transform duration-300 text-center">
+                <Image
+                    src={product.imageUrl}
+                    alt={product.name}
+                    width={100}
+                    height={100}
+                    className="w-full h-48 object-contain rounded-md mx-auto" />
+                <div className="mt-4">
+                    <h2 className="text-lg font-semibold text-gray-900">{product.name}</h2>
+                    <p className="text-sm text-gray-500">{product.brand}</p>
+                    <p>{product.unit}</p>
+                    <p className="mt-2 text-xl font-bold text-gray-900">{product.defaultPrice} €</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 export default ProductCard;
