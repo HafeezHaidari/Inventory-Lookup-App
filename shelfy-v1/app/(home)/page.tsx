abe3867038cfd3bc39b81ef048a1e288307a3d32
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-12 23:27:15 +0200

    Moved AppLogo and SearchBar to layout.tsx.

--- shelfy-v1/app/(home)/page.tsx
+++ shelfy-v1/app/(home)/page.tsx
@@ -1,12 +1,8 @@
-import AppLogo from "@/components/AppLogo";
-import SearchBar from "@/components/SearchBar";
 import LandingProductsPresenter from "@/components/LandingProductsPresenter";
 
 export default function Home() {
   return (
       <>
-          <AppLogo />
-          <SearchBar />
           <LandingProductsPresenter />
       </>
   );
