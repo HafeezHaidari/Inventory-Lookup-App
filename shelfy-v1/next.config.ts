21511dc772b981a00f7688f47babdf988b4510a8
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-12 23:23:48 +0200

    Added pcisum.photos for dev, can be removed later.

--- shelfy-v1/next.config.ts
+++ shelfy-v1/next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
             {
                 protocol: "https",
                 hostname: "m.media-amazon.com",
-            },
+            }, {
+                protocol: "https",
+                hostname: "picsum.photos",
+            }
         ],
     },
 };
