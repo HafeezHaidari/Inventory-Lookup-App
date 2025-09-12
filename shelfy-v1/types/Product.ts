065de542f9594def17c374a6ce0042f424f8441e
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-12 23:27:43 +0200

    Added is_recommended.

--- shelfy-v1/types/Product.ts
+++ shelfy-v1/types/Product.ts
@@ -1,7 +1,10 @@
 export interface Product {
+    id: number;
     name: string,
     brand: string,
     unit: string,
     defaultPrice: number,
     imageUrl: string,
+    is_recommended: boolean,
 }
+
