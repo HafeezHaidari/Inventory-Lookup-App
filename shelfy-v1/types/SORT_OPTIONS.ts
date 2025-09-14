a376c429e976f827a4cb15f71a90b5f6ef26d1ea
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-14 19:06:47 +0200

    Options for sorting to be compliant with the backend

---
+++ shelfy-v1/types/SORT_OPTIONS.ts
@@ -0,0 +1,7 @@
+export const SORT_OPTIONS = [
+    { label: "Name A–Z", value: "name,asc" },
+    { label: "Name Z–A", value: "name,desc" },
+    { label: "Price ↑",  value: "defaultPrice,asc" },
+    { label: "Price ↓",  value: "defaultPrice,desc" },
+    { label: "Brand A–Z", value: "brand,asc" },
+];
