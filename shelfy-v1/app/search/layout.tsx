703245b2e11c94ab44d4c26ff9c21b43ae3c0f6c
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-09-14 19:02:15 +0200

    Created a layout.tsx for searches. It was to display the filterbar only on searches.

---
+++ shelfy-v1/app/search/layout.tsx
@@ -0,0 +1,21 @@
+// app/search/layout.tsx
+import React from "react";
+import FilterBar from "@/components/masterdetail/FilterBar";
+
+export default function SearchLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex h-full min-h-0 flex-col">
+            {/* FilterBar lives directly under the global SearchBar */}
+            <div className="border-b px-4 py-2">
+                <div className="sticky top-0 z-10 bg-white border-b px-4 py-2">
+                    <FilterBar />
+                </div>
+            </div>
+
+            {/* The rest of the /search content (master/detail) */}
+            <div className="flex-1 min-h-0">
+                {children}
+            </div>
+        </div>
+    );
+}
