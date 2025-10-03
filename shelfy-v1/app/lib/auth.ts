e48693fc1a91ebe501cd62764f80b090e37e9484
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-03 18:07:38 +0200

    Added logout function

--- shelfy-v1/app/lib/auth.ts
+++ shelfy-v1/app/lib/auth.ts
@@ -1,20 +1,31 @@
-import {FormEvent} from "react";
+import { useRouter } from "next/navigation";
+
+const base = process.env.NEXT_PUBLIC_API_BASE;
 
 export const loginWithFormData = async (formData: FormData) => {
+
     const username = String(formData.get("username") ?? "");
     const password = String(formData.get("password") ?? "");
 
-    const base = process.env.NEXT_PUBLIC_API_BASE; // expose to client
     const response = await fetch(`${base}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        credentials: "include", // if your backend sets cookies
+        credentials: "include",
         body: JSON.stringify({ username, password }),
     });
 
     if (!response.ok) {
         throw new Error("Login failed");
-    } else {
-        console.log(response);
     }
+
+    window.location.reload();
+}
+
+export const logout = async () => {
+
+    await fetch(`${base}/auth/logout`, {
+        method: "POST",
+        credentials: "include"
+    });
+    window.location.reload();
 }
