5f5793fee859ff010cbb04380f8767bc2b2d47fd
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-03 18:08:56 +0200

    Wrapped page inside a SessionProvider element

--- shelfy-v1/app/layout.tsx
+++ shelfy-v1/app/layout.tsx
@@ -3,8 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppLogo from "@/components/AppLogo";
 import SearchBar from "@/components/SearchBar";
-import { LoginButton } from "@/components/LoginForm";
 import Link from "next/link";
+import AuthSection from "@/components/AuthSection";
+import SessionProvider from "@/app/lib/SessionProvider";
+import {getSession} from "@/app/lib/session";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,32 +23,35 @@ export const metadata: Metadata = {
   description: "Online tool to search an inventory",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const user = await getSession();
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden flex flex-col`}
       >
-      <header className="shrink-0">
-          <div className="flex items-center justify-between gap-4 px-4 py-2 ">
-              <div className="mb-16">
-                  <LoginButton />
+      <SessionProvider initialSession={user ? { isAuthenticated: true, user} : {isAuthenticated: false, user: null}}>
+          <header className="shrink-0">
+              <div className="flex items-center justify-between gap-4 px-4 py-2 ">
+                  <div className="mb-16">
+                      <AuthSection />
+                  </div>
+                  <div className="flex flex-col items-center gap-1 justify-center items w-full">
+                      <Link href={"/"}>
+                          <AppLogo />
+                      </Link>
+                      <SearchBar />
+                  </div>
               </div>
-              <div className="flex flex-col items-center gap-1 justify-center items w-full">
-                  <Link href={"/"}>
-                      <AppLogo />
-                  </Link>
-                  <SearchBar />
-              </div>
-          </div>
-      </header>
-      <main className="flex-1 min-h-0">
-          {children}
-      </main>
+          </header>
+          <main className="flex-1 min-h-0">
+              {children}
+          </main>
+        </SessionProvider>
       </body>
     </html>
   );
