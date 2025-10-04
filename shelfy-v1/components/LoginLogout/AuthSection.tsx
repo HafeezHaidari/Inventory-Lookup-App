116f110f405b949fc58ee7faafaded883d37d528
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-04 23:00:57 +0200

    Added styling and modified imports.

--- shelfy-v1/components/LoginLogout/AuthSection.tsx
+++ shelfy-v1/components/LoginLogout/AuthSection.tsx
@@ -1,12 +1,20 @@
 'use client';
 import { useSession } from '@/app/lib/SessionProvider';
-import {LoginForm} from "@/components/LoginLogout/LoginForm";
+import { LoginForm } from "@/components/LoginLogout/LoginForm";
+import { logout } from "@/app/lib/auth";
+import LogoutButton from "@/components/LoginLogout/LogoutButton";
+import CreateButton from "@/components/CreateButton";
 
 export default function AuthSection() {
     const { isAuthenticated } = useSession();
 
     if (isAuthenticated) {
-        return <div>Welcome!</div>
+        return (
+            <div className="flex items-center justify-center gap-3">
+                <LogoutButton onClick={logout} />
+                <CreateButton />
+            </div>
+        );
     }
 
     return <LoginForm />;
