732fb19f911cc37c14284974ed05ca16eb765e1a
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-04 22:59:53 +0200

    Created a CreateButton.tsx component that redirects to the /create page.

---
+++ shelfy-v1/components/CreateButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router';
+import Link from "next/link";
+
+const CreateButton = () => {
+
+    return (
+        <Link href="/create">
+            <button className="px-4 py-2 bg-emerald-600 text-white rounded hover:bg-emerald-700 transition">
+                Create
+            </button>
+        </Link>
+    )
+}
+export default CreateButton
