e104ceebc759bd933eca7752e52807e0450751a7
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-04 23:01:43 +0200

    Created Logout button component.

---
+++ shelfy-v1/components/LoginLogout/LogoutButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface LoginButtonProps {
+    onClick: () => void;
+}
+
+const LoginButton = ({ onClick }: LoginButtonProps) => {
+
+    return (
+
+        <button onClick={onClick} className="px-4 py-2 bg-emerald-600 text-white rounded hover:bg-emerald-700 transition">
+            Logout
+        </button>
+
+    )
+}
+export default LoginButton
