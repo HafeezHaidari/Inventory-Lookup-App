1ffa57dfb9b7ad74a3f322ece3fed5de237f6b6e
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-09 00:19:23 +0200

    Added cloudinary host for images.

--- shelfy-v1/next.config.ts
+++ shelfy-v1/next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
             }, {
                 protocol: "https",
                 hostname: "picsum.photos",
+            },
+            {
+                protocol: "https",
+                hostname: "example.com",
+            },
+            {
+                protocol: "http",
+                hostname: "res.cloudinary.com",
             }
         ],
     },
