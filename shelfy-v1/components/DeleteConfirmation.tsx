41f049478b2dce25a8386397a628d58b1c3664a0
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-12 00:10:36 +0200

    Added the Delete Button and the PopupModal.tsx.

--- shelfy-v1/components/DeleteConfirmation.tsx
+++ shelfy-v1/components/DeleteConfirmation.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import DeleteButton from "@/components/DeleteButton";
+import PopupModal from "@/components/LoginLogout/PopupModal";
 
-const DeleteConfirmation = () => {
+interface Props {
+    id: number;
+
+}
+
+const DeleteConfirmation = ({ id }: Props) => {
+    const [openPopUp, setOpenPopUp] = useState(false);
+
+    const handleClick = () => setOpenPopUp(false);
+
+
     return (
-        <div>DeleteConfirmation</div>
+        <>
+            <DeleteButton onClick={() => setOpenPopUp(true)}/>
+            <PopupModal isOpen={openPopUp} handleClose={() => setOpenPopUp(false)}>
+                <div className="space-x-5">
+                    <button onClick={() => setOpenPopUp(false)}>
+                        Cancel
+                    </button>
+                    <button onClick={handleClick}>
+                        Delete
+                    </button>
+                </div>
+            </PopupModal>
+        </>
+
     )
 }
 export default DeleteConfirmation
