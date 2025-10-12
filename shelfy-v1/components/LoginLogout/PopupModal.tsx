e9ac48d6ccf8dc75a8167de2046b740c3007cad4
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-12 20:14:42 +0200

    Redesigned popup box.

--- shelfy-v1/components/LoginLogout/PopupModal.tsx
+++ shelfy-v1/components/LoginLogout/PopupModal.tsx
@@ -35,8 +35,8 @@ const PopupModal = ({
         <ReactPortal wrapperId='react-portal-login-popup-container'>
             <>
                 <div onClick={handleClose} className="fixed top-0 left-0 w-screen h-screen z-40 bg-neutral-800 opacity-50" aria-hidden="true"/>
-                <div className="fixed rounded flex flex-col box-border min-w-fit overflow-hidden p-5 bg-white top-32 inset-x-12 sm:inset-x-24 md:inset-x-1/4 sm:top-1/5 z-50 opacity-100">
-                    <div onClick={(e) => e.stopPropagation()} className="box-border h-5/6">{children}</div>
+                <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 rounded-xl bg-white p-5 shadow-xl w-fit min-w-[100px] max-w-[90vw] max-h-[80vh] overflow-y-auto">
+                    <div onClick={(e) => e.stopPropagation()} className="box-border">{children}</div>
                 </div>
             </>
         </ReactPortal>
