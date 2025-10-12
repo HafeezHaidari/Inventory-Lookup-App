888c2e0e358fee4f8aa6b302f4819b43e5bacee4
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-12 20:39:37 +0200

    Redesigned name box.

--- shelfy-v1/components/ProductCard.tsx
+++ shelfy-v1/components/ProductCard.tsx
@@ -17,7 +17,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                 height={100}
                 className="w-full h-48 object-contain rounded-md mx-auto" />
             <div className="mt-4">
-                <h2 className="text-lg font-semibold text-gray-900">{product.name}</h2>
+                <h2 className="text-lg font-semibold text-gray-900 truncate h-7">{product.name}</h2>
                 <p className="text-sm text-gray-500">{product.brand}</p>
                 <p>{product.unit}</p>
                 <p className="mt-2 text-xl font-bold text-gray-900">{product.defaultPrice} €</p>
