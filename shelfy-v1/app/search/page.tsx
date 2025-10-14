30bb4fc4b48b8adbac706056c0b1ccd5525fa5eb
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-14 18:33:28 +0200

    Set revalidation time to 1 second.

--- shelfy-v1/app/search/page.tsx
+++ shelfy-v1/app/search/page.tsx
@@ -42,7 +42,7 @@ const fetchProducts = async (query?: string, tab?: string, filters?: Filters) =>
     const url = qs ? `${base}?${qs}` : base;
 
     // Fetch products from the API with revalidation every second
-    const res = await fetch(url, { next: { revalidate: 0 } });
+    const res = await fetch(url, { next: { revalidate: 1 } });
     if (!res.ok) throw new Error("Fetch failed");
     const data = await res.json();
 
