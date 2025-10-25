c68eb82fbd80e5655064b1717f9bbf506d281667
Hafeezullah Haidari <107349471+HafeezHaidari@users.noreply.github.com>
2025-10-25 22:04:20 +0200

    Simple formatting

--- shelfy-v1/middleware.ts
+++ shelfy-v1/middleware.ts
@@ -3,11 +3,9 @@ import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     const hasAccess = Boolean(req.cookies.get('accessToken'));
-    const isProtected = req.nextUrl.pathname.startsWith("/create");
+    const isProtected = req.nextUrl.pathname.startsWith('/create');
 
-    if (isProtected && !hasAccess) {
-        const url = new URL("/", req.url);
-        return NextResponse.redirect(url)
-    }
+    if (isProtected && !hasAccess) return NextResponse.redirect(new URL('/', req.url));
+
     return NextResponse.next();
 }
